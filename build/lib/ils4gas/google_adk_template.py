6f6c2d308b126c0ed343c548ff99cfe096e657b6
root <pxlxingliang>
2025-07-22 17:49:13 +0800

    add a demo

---
+++ build/lib/ils4gas/google_adk_template.py
@@ -0,0 +1,57 @@
+from ils4gas.env import get_llm_config
+import os
+
+def gen_google_adk_template():
+    """
+    Generate a Google ADK template for ils4gas.
+    
+    This function sets up the environment and initializes the agent with the necessary tools.
+    """
+    llm_model, llm_api_key, llm_base_url = get_llm_config()
+    port = os.environ.get("ILS4GAS_PORT", "50001")
+    transport = os.environ.get("ILS4GAS_TRANSPORT", "sse")
+    if transport == "sse":
+        mcp_toolset = "SseServerParams"
+        url = f"http://localhost:{port}/see"
+    elif transport == "streamable-http":
+        mcp_toolset = "StreamableHTTPServerParams"
+        url = f"http://localhost:{port}/mcp"
+    else:
+        raise ValueError("Invalid transport protocol specified. Use 'sse' or 'streamable-http'.")
+
+    template = f"""
+from google.adk.agents import Agent
+from google.adk.models.lite_llm import LiteLlm
+from google.adk.tools.mcp_tool.mcp_session_manager import SseServerParams, StreamableHTTPServerParams
+from dp.agent.adapter.adk import CalculationMCPToolset
+
+toolset = CalculationMCPToolset(
+    connection_params={mcp_toolset}(
+        url="{url}",
+    )
+)
+
+model = LiteLlm(
+    model={repr(llm_model)},
+    api_key={repr(llm_api_key)},
+    base_url={repr(llm_base_url)}
+)
+root_agent = Agent(
+    name='agent',
+    model=model,
+    description=(
+        "ils4gas agent"
+    ),
+    instruction="You are an agent that can use tools to solve problems",
+    tools=[tools]
+)
+    """
+    
+    os.makedirs("myagent", exist_ok=True)
+    with open("myagent/agent.py", "w") as f:
+        f.write(template)
+    with open("myagent/__init__.py", "w") as f:
+        f.write("from . import agent")
+        
+    print("Google ADK template generated, please type below command to run the agent:")
+    print("    adk web --host 0.0.0.0 --port 50002\n")
---
+++ build/lib/ils4gas/main.py
@@ -0,0 +1,97 @@
+import argparse
+import os
+
+from ils4gas.env import set_envs
+from ils4gas.google_adk_template import gen_google_adk_template
+
+from importlib.metadata import version
+__version__ = version("ils4gas")
+
+def parse_args():
+    """
+    Parse command line arguments.
+    """
+    parser = argparse.ArgumentParser(description="AbacusAgent Command Line Interface")
+    
+    parser.add_argument(
+        "--transport",
+        type=str,
+        default=None,
+        choices=["sse", "streamable-http"],
+        help="Transport protocol to use (default: sse), choices: sse, streamable-http"
+    )
+    parser.add_argument(
+        "--port",
+        type=int,
+        default=None,
+        help="Port to run the MCP server on (default: 50001)"
+    )
+    parser.add_argument(
+        "--host",
+        type=str,
+        default=None,
+        help="Host to run the MCP server on (default: localhost)"
+    )
+    parser.add_argument(
+        "--version",
+        action="store_true",
+        help="Print the version of Ils4gasAgentTools"
+    )
+    parser.add_argument(
+        "--template",
+        action="store_true",
+        help="Generate a Google ADK template for ils4gas"
+    )
+    
+    args = parser.parse_args()
+    
+    return args
+
+def print_version():
+    """
+    Print the version.
+    """
+    print(f"\nIls4gasAgentTools Version: {__version__}")
+    print("For more information, visit: https://gitee.com/pxlxingliang/ils4gas\n")
+    
+def print_address():
+    """
+    Print the address of the MCP server based on environment variables.
+    """
+    address = f"{os.environ['ILS4GAS_HOST']}:{os.environ['ILS4GAS_PORT']}"
+    if os.environ["ILS4GAS_TRANSPORT"] == "sse":
+        print("Address:", address + "/sse")
+    elif os.environ["ILS4GAS_TRANSPORT"] == "streamable-http":
+        print("Address:", address + "/mcp")
+    else:
+        raise ValueError("Invalid transport protocol specified. Use 'sse' or 'streamable-http'.")
+
+def main():
+    """
+    Main function to run the MCP tool.
+    """
+    print_version()
+    args = parse_args()  
+
+    set_envs(
+        transport_input=args.transport,
+        port_input=args.port, 
+        host_input=args.host)
+
+    if args.version:
+        print_version()
+        return
+    
+    if args.template:
+        gen_google_adk_template()
+        return
+    
+    from ils4gas.init_mcp import mcp
+    from ils4gas.env import load_tools
+    load_tools()  
+
+    print_address()
+    mcp.run(transport=os.environ["ILS4GAS_TRANSPORT"])
+
+if __name__ == "__main__":
+    main()
